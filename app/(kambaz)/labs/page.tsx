3add3955b8e7f620c8565428979488033d6ab864
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2026-01-20 17:57:49 -0500

    update

--- app/(kambaz)/labs/page.tsx
+++ app/(kambaz)/labs/page.tsx
@@ -1,26 +1,19 @@
-import Link from "next/link";
+import TOC from "@/app/labs/TOC";
 
 export default function Labs() {
   return (
     <div id="wd-labs">
       <h1>Labs</h1>
-      <ul>
-        <li>
-          <Link href="/labs/lab1" id="wd-lab1-link">
-            Lab 1: HTML Examples
-          </Link>
-        </li>
-        <li>
-          <Link href="/labs/lab2" id="wd-lab2-link">
-            Lab 2: CSS Basics
-          </Link>
-        </li>
-        <li>
-          <Link href="/labs/lab3" id="wd-lab3-link">
-            Lab 3: JavaScript Fundamentals
-          </Link>
-        </li>
-      </ul>
+      <p>Kailin Xing</p>
+      <TOC />
+      <a
+        href="https://github.com/kailinxGitHub/kambaz-next-js"
+        id="wd-github"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub Repository
+      </a>
     </div>
   );
 }
