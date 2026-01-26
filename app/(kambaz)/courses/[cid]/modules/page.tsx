a1dd40d1e7cf6d74b8dc7b47b9b0c5a8632c3752
kailinxGitHub <67965369+kailinxGitHub@users.noreply.github.com>
2026-01-25 21:51:44 -0500

    cleanup

--- app/(kambaz)/courses/[cid]/modules/page.tsx
+++ app/(kambaz)/courses/[cid]/modules/page.tsx
@@ -1,6 +1,12 @@
 export default function Modules() {
   return (
     <div>
+      <button>Collapse All</button>
+      <button>View Progress</button>
+      <select>
+        <option>Publish All</option>
+      </select>
+      <button>+ Module</button>
       <ul id="wd-modules">
         <li className="wd-module">
           <div className="wd-title">Week 1</div>
@@ -9,38 +15,13 @@ export default function Modules() {
               <span className="wd-title">LEARNING OBJECTIVES</span>
               <ul className="wd-content">
                 <li className="wd-content-item">Introduction to the course</li>
-                <li className="wd-content-item">
-                  Learn what is Web Development
-                </li>
+                <li className="wd-content-item">Learn what is Web Development</li>
               </ul>
             </li>
           </ul>
         </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 2</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">HTML FORMS</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Creating forms</li>
-                <li className="wd-content-item">Form validation</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li className="wd-module">
-          <div className="wd-title">Week 3</div>
-          <ul className="wd-lessons">
-            <li className="wd-lesson">
-              <span className="wd-title">CSS BASICS</span>
-              <ul className="wd-content">
-                <li className="wd-content-item">Styling elements</li>
-                <li className="wd-content-item">CSS selectors</li>
-              </ul>
-            </li>
-          </ul>
-        </li>
+        <li className="wd-module"> <div className="wd-title">Week 2</div> </li>
+        <li className="wd-module"> <div className="wd-title">Week 3</div> </li>
       </ul>
     </div>
-  );
-}
+);}
